4af02ab1443f2e3e87e31b69fe06bc927fd9c175
lthsds <779021596@qq.com>
2026-01-12 20:57:59 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,42 +84,6 @@ fun allCommitted(): Boolean {
     }
 }
 
-val keyProps = Properties()
-val keyPropsFile: File = rootProject.file("keystore/keystore.properties")
-keyProps.load(FileInputStream(keyPropsFile))
-fun getStoreFile(): String {
-    var storeFile = keyProps["storeFile"].toString()
-    if (storeFile.isEmpty()) {
-        storeFile = System.getenv("storeFile") ?: ""
-    }
-    return storeFile
-}
-
-fun getStorePassword(): String {
-    var storePassword = keyProps["storePassword"].toString()
-    if (storePassword.isEmpty()) {
-        storePassword = System.getenv("storePassword") ?: ""
-    }
-    return storePassword
-}
-
-fun getKeyAlias(): String {
-    var keyAlias = keyProps["keyAlias"].toString()
-    if (keyAlias.isEmpty()) {
-        keyAlias = System.getenv("keyAlias") ?: ""
-    }
-    return keyAlias
-}
-
-fun getKeyPassword(): String {
-    var keyPassword = keyProps["keyPassword"].toString()
-    if (keyPassword.isEmpty()) {
-        keyPassword = System.getenv("keyPassword") ?: ""
-    }
-    return keyPassword
-}
-
-
 android {
 
     namespace = "app.aaps"
@@ -174,31 +138,6 @@ android {
             manifestPlaceholders["appIconRound"] = "@mipmap/ic_blueowl"
         }
     }
-
-    signingConfigs {
-        create("release") {
-            storeFile = file(getStoreFile())
-            storePassword = getStorePassword()
-            keyAlias = getKeyAlias()
-            keyPassword = getKeyPassword()
-        }
-    }
-
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.findByName("release")
-        }
-
-    }
-
-    useLibrary("org.apache.http.legacy")
-
-    //Deleting it causes a binding error
-    buildFeatures {
-        dataBinding = true
-        buildConfig = true
-    }
-}
 
 allprojects {
     repositories {
